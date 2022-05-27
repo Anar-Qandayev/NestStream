34117a17ff92923fb36001cecb76bb48a6c1c5ab
Anar-Qandayev <tu7bxfga8@code.edu.az>
2022-05-27 11:48:44 +0400

    commit

---
+++ NestStream/DAL/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nest.Models;
+using NestStream.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NestStream.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+    }
+}
---
+++ NestStream/ViewModels/HomeVM.cs
@@ -0,0 +1,18 @@
+﻿using Nest.Models;
+using NestStream.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NestStream.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Slider> Sliders { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+        public List<Product> RecentProducts { get; set; }
+        public List<Product> TopRatedProducts { get; set; }
+    }
+}
